fada38f2b258230291c3742797669906857614d7
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-20 08:54:12 +0100

    Corrected js content

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@ $(function() {
 	var carouselList = $('#carousel ul');
 	setInterval(changeSlide, 3000);
 	function changeSlide() {
-	carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+		carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
 	}
 	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	}
